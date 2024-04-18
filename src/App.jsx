a4afc9f544e3101d33347bce046332c71c523c7d
Yukihira2k <89770909+Yukihira2k@users.noreply.github.com>
2024-04-18 18:34:23 +0800

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Benefits from "./Components/Benefits";
+import Collaboration from "./Components/Collaboration";
+import Services from "./Components/Services";
+import Pricing from "./Components/Pricing";
+import Roadmap from "./Components/Roadmap";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
@@ -8,6 +14,12 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
